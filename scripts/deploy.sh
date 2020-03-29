5026c89e63b3d8422d842b679c9ca0caea82cdb1
AlexeyTuboltsev <tblz@palavara.com>
2020-03-29 19:40:50 +0200

    add deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+bucketName="$(dotenv get BUCKET_NAME)"
+distributionId="$(dotenv get DISTRIBUTION_ID)"
+
+echo "--deploying to $bucketName --"
+
+aws s3 sync build/ s3://$bucketName
+aws cloudfront create-invalidation --distribution-id $distributionId --invalidation-batch file://scripts/invalidation-batch.json
